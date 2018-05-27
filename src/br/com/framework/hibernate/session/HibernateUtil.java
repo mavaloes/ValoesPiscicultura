d00659f7f2a2b258db01688d961c10afc048fa9e
Valoes <Valoes@MarcoValoes>
2018-05-27 19:38:50 -0300

    Projeto valoes piscicultura

--- src/br/com/framework/hibernate/session/HibernateUtil.java
+++ src/br/com/framework/hibernate/session/HibernateUtil.java
@@ -17,77 +17,76 @@ import org.hibernate.engine.SessionFactoryImplementor;
 import br.com.framework.implementacao.crud.VariavelConexaoUtil;
 
 /**
- * Responsavel por estabelecer a conex„o com hibernate
+ * Responsavel por estabelecer a conex√£o com hibernate
+ * 
  * @author alex
  *
  */
 @ApplicationScoped
 public class HibernateUtil implements Serializable {
-	
+
 	private static final long serialVersionUID = 1L;
 
 	public static String JAVA_COMP_ENV_JDBC_DATA_SOURCE = "java:/comp/env/jdbc/datasource";
-	
+
 	private static SessionFactory sessionFactory = buildSessionFactory();
-	
+
 	/**
-	 * Respons·vel por ler o arquivo de configuraÁ„o hibernate.cfg.xml
+	 * Respons√°vel por ler o arquivo de configura√ß√£o hibernate.cfg.xml
+	 * 
 	 * @return SessionFactory
 	 */
-	private static SessionFactory buildSessionFactory(){
-		
+	private static SessionFactory buildSessionFactory() {
+
 		try {
-			
-			if (sessionFactory == null){
+			if (sessionFactory == null) {
 				sessionFactory = new Configuration().configure().buildSessionFactory();
 			}
-			
 			return sessionFactory;
-		}catch (Exception e){
+		} catch (Exception e) {
 			e.printStackTrace();
-			throw new ExceptionInInitializerError("Erro ao criar conex„o SessionFactory");
+			throw new ExceptionInInitializerError("Erro ao criar conex√£o SessionFactory");
 		}
-		
 	}
-	
-	
+
 	/**
 	 * Retorna o SessionFactory corrente
+	 * 
 	 * @return SessionFactory
 	 */
-	public static SessionFactory getSessionFactory(){
+	public static SessionFactory getSessionFactory() {
 		return sessionFactory;
 	}
-	
+
 	/**
-	 * Retorna a sess„o do SessiontFactory
+	 * Retorna a sess√£o do SessiontFactory
+	 * 
 	 * @return Session
 	 */
-	public static Session getCurrentSession(){
+	public static Session getCurrentSession() {
 		return getSessionFactory().getCurrentSession();
 	}
-	
+
 	/**
-	 * Abre uma nova sess„o no SessionFactory
-	 * return Session
-	 */ 
-	public static Session openSession(){
-		if (sessionFactory == null){
+	 * Abre uma nova sess√£o no SessionFactory return Session
+	 */
+	public static Session openSession() {
+		if (sessionFactory == null) {
 			buildSessionFactory();
 		}
-		
 		return sessionFactory.openSession();
 	}
-	
+
 	/**
-	 * Obtem a conection do provedor de conexıes configurado
+	 * Obtem a conection do provedor de conex√µes configurado
+	 * 
 	 * @return Connection SQL
 	 * @throws SQLException
 	 */
-	public static Connection getConnectionProvider () throws SQLException {
+	public static Connection getConnectionProvider() throws SQLException {
 		return ((SessionFactoryImplementor) sessionFactory).getConnectionProvider().getConnection();
 	}
-	
+
 	/**
 	 * 
 	 * @return Connection no InitialContext java:/comp/env/jdbc/datasource
@@ -98,16 +97,15 @@ public class HibernateUtil implements Serializable {
 		DataSource ds = (DataSource) context.lookup(JAVA_COMP_ENV_JDBC_DATA_SOURCE);
 		return ds.getConnection();
 	}
-	
+
 	/**
 	 * 
 	 * @return DataSource JNDI Tomcat
 	 * @throws NamingException
 	 */
-	public DataSource getDataSourceJndi () throws NamingException {
+	public DataSource getDataSourceJndi() throws NamingException {
 		InitialContext context = new InitialContext();
 		return (DataSource) context.lookup(VariavelConexaoUtil.JAVA_COMP_ENV_JDBC_DATA_SOURCE);
 	}
-	
 
 }
